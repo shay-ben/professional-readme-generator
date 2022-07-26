87e2c8f39240a0d51e527788def582742b8ce446
Your Name <you@example.com>
2022-07-26 12:01:20 -0400

    changes files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generateMarkdown = require("./generateMarkdown.js");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 function userInput(){
